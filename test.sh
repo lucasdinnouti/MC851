3d68686bb3af3c56a34166df2b827673b6b7db68
gabrielpallotta <gabriel.cardenete@hotmail.com>
2023-08-16 22:25:11 -0300

    Organize folders and add simple test script

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Running ALU tests"
+iverilog src/constants.v src/assert.v src/alu.v test/alu_tb.v && ./a.out
+
+echo "Running decoder tests"
+iverilog src/constants.v src/assert.v src/decoder.v test/decoder_tb.v && ./a.out
